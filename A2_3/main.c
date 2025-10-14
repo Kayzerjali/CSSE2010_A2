9dc93aac09dbb5124f08f9e5416b2315b2fb281e
Kayzer Ali <kegselmon2000@gmail.com>
2025-10-15 09:17:01 +1000

    Initial commit

---
+++ A2_3/main.c
@@ -0,0 +1,18 @@
+/*
+ * A2_3.c
+ *
+ * Created: 15/10/2025 9:09:22 AM
+ * Author : Kayzer
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
