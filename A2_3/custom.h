6d50f911bd96711c81dfc6bb26cf2b594c4595fe
Kayzer Ali <kegselmon2000@gmail.com>
2025-10-15 10:12:52 +1000

    added custom header file

---
+++ A2_3/custom.h
@@ -0,0 +1,25 @@
+
+#ifndef 
+
+// SSD encodings
+
+typedef enum {
+	SSD_ZERO = 0x3F,
+	SSD_ONE = 0x06,
+	SSD_TWO = 0x5B,
+	SSD_THREE = 0x4F,
+	SSD_FOUR = 0x66,
+	SSD_FIVE = 0x6D,
+	SSD_SIX = 0x7D,
+	SSD_SEVEN = 0x07,
+	SSD_EIGHT = 0x7F,
+	SSD_NINE = 0x6F,
+	SSD_Q = 0x67,
+	SSD_P = 0x73,
+	SSD_B = 0x7C,
+	SSD_D = 0x5E
+} SSD_SYMBOL;
+
+
+
+#endif
