ee4623f964a930fff799cb4d8b2a49b0860db62d
Kayzer Ali <kegselmon2000@gmail.com>
2025-10-16 12:05:31 +1000

    change git ignore

--- A2_3/Debug/makedep.mk
+++ A2_3/Debug/makedep.mk
@@ -2,5 +2,13 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-main.c
+ledmatrix.c
+
+microwaveAssessment.c
+
+serialio.c
+
+spi.c
+
+terminalio.c
 
--- A2_3/custom.h
+++ A2_3/custom.h
@@ -1,9 +1,26 @@
 
 #ifndef 
 
-// SSD encodings
+/* ===== ENUMS ===== */
 
+// Mode enumeration
 typedef enum {
+	MODE_QUICK,
+	MODE_POPCORN,
+	MODE_BEVERAGE,
+	MODE_DEFROST
+} Mode;
+
+// Power level enumeration
+typedef enum {
+	POWER_10,
+	POWER_50,
+	POWER_100
+} PowerLevel;
+
+// SSD encodings
+typedef enum {
+	
 	SSD_ZERO = 0x3F,
 	SSD_ONE = 0x06,
 	SSD_TWO = 0x5B,
@@ -18,6 +35,7 @@ typedef enum {
 	SSD_P = 0x73,
 	SSD_B = 0x7C,
 	SSD_D = 0x5E
+	
 } SSD_SYMBOL;
 
 
